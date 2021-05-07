beb65f7e793c874ad7e187f3622bf6acc1f4c58c
Robingorski <Robingorski@hotmail.com>
2021-05-07 15:53:19 +0200

    added new picture for KYH-case, removed dead code, starting to code on 'pageindicator', put a z-index on pgae indicator, and removed 'sections.js'

---
+++ src/component/Pageindicator.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const items = [0, 1, 2, 3, 4, 5];
+
+const Pageindicator = () => {
+    const [index, setIndex] = useState(0);
+    const increment = useRef(1);
+    useEffect(() => {
+      let interval = setInterval(() => {
+        if (index >= items.length - 1) {
+          increment.current = -1;
+        } else if (index <= 0) {
+          increment.current = 1;
+        }
+        setIndex(index + increment.current);
+      }, 500);
+      return () => clearInterval(interval);
+    }, []);
+    
+
+  return (
+    <div className="App">
+      <div className="background">
+        {items.map((i) => {
+          return (
+              <div className="page_indicator_container">
+              <div className="page_indicator-qs"></div>
+              <div className="page_indicator-quire"></div>
+              <div className="page_indicator-sss"></div>
+              <div className="page_indicator-kyh"></div>
+              <div className="page_indicator-about_me"></div>
+              <div className="mover"style={{transform: `translateX(${(index - i) * 40}px)`,}}/>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+
+  export default Pageindicator;
